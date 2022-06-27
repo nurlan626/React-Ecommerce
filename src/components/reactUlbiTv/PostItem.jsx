01ad7d501ce8eb8d3be153d0689858c51e119053
nurlan007 <nurlan@artifactlabs.io>
2022-06-27 20:50:44 +0400

    fix

--- src/components/reactUlbiTv/PostItem.jsx
+++ src/components/reactUlbiTv/PostItem.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { addToCart } from "../../redux/cartReducer";
 import MyButton from "../UI/button/MyButton";
+import './PostItem.scss'
 
 const PostItem = (props) => {
   const navigate = useNavigate();
+  const user = useSelector(state => state.user)
+  const dispatch = useDispatch()
   const removePost = () => {
     props.removePost(props.post);
   };
   return (
-    <div>
       <div className="post">
         <div className="post__contnent">
-          {/* <div>{props.number}.</div> */}
           <div>{props.post.id}</div>
           <div>
             <div>
@@ -19,19 +22,13 @@ const PostItem = (props) => {
             </div>{" "}
             {props.post.title}
           </div>
-          {/* <div>
-            <div>
-              <u>body: </u>
-            </div>
-            {props.post.body}
-          </div> */}
         </div>
         <div className="post__btns">
           <MyButton onClick={() => navigate(`/allPosts/${props.post.id}`)}>Open</MyButton>
           <MyButton onClick={removePost}>Delete</MyButton>
+          {user && (<MyButton onClick={() => dispatch(addToCart(props.post))}>add to cart</MyButton>)}
         </div>
       </div>
-    </div>
   );
 };
 
--- src/components/reactUlbiTv/PostList.jsx
+++ src/components/reactUlbiTv/PostList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Loader from "../UI/loader/Loader";
 import PostItem from "./PostItem";
+import "./PostList.scss"
 
 const PostList = ({ posts, removePost, loading }) => {
   return (
@@ -11,23 +12,27 @@ const PostList = ({ posts, removePost, loading }) => {
       ) : (
         <div>
           <h1>List of posts</h1>
+          <div className="xxx">
           <TransitionGroup>
-            {posts.length > 0 ? (
-              posts.map((post, index) => {
-                return (
-                  <CSSTransition key={post.id} timeout={500} classNames="post">
-                    <PostItem
-                      removePost={removePost}
-                      number={index + 1}
-                      post={post}
-                    />
-                  </CSSTransition>
-                );
-              })
-            ) : (
-              <div>Постов нет</div>
-            )}
-          </TransitionGroup>
+           
+           {posts.length > 0 ? (
+             posts.map((post, index) => {
+               return (
+                 <CSSTransition key={post.id} timeout={500} classNames="post">
+                   <PostItem
+                     removePost={removePost}
+                     number={index + 1}
+                     post={post}
+                   />
+                 </CSSTransition>
+               );
+             })
+           ) : (
+             <div>Постов нет</div>
+           )}
+         </TransitionGroup>
+              </div>
+          
         </div>
       )}
     </div>
